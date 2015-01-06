07766de97414039d9836a7a92940a180432f3e4c
Enric Ribas <dev+enric@influitive.com>
2015-01-05 21:37:04 -0500

    Create postgres user

--- recipes/infl/recipes/create_psql_root_user.rb
+++ recipes/infl/recipes/create_psql_root_user.rb
@@ -1,12 +1,24 @@
-execute 'create a database for the current user' do
-  command "/usr/local/bin/createdb -U postgres"
-  user "root"
-  not_if "psql -U postgres -c 'select 1' postgres &> /dev/null"
+execute "create-root-user" do
+  code = <<-EOH
+    psql -U postgres -c "select * from pg_user where usename='root'" | grep -c root
+  EOH
+  command "createuser -U postgres -s root"
+  not_if code
 end
 
-execute 'create the postgres superuser' do
-  command "/usr/local/bin/createuser -U postgres --superuser root"
-  user "root"
-  not_if 'psql -U postgres -c "select 1" &> /dev/null'
+execute "create-database-user" do
+  code = <<-EOH
+    psql -U postgres -c "select * from pg_user where usename='#{node[:dbuser]}'" | grep -c #{node[:dbuser]}
+  EOH
+  command "createuser -U postgres -sw #{node[:dbuser]}"
+  not_if code
+end
+
+execute "create-database" do
+  exists = <<-EOH
+    psql -U postgres -c "select * from pg_database WHERE datname='#{node[:dbname]}'" | grep -c #{node[:dbname]}
+  EOH
+  command "createdb -U postgres -O #{node[:dbuser]} -E utf8 -T template0 #{node[:dbname]}"
+  not_if exists
 end
 
