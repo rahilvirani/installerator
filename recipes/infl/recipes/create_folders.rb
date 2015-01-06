31682badf732953e8048f55b5610ac0b66d9bc00
Enric Ribas <dev+enric@influitive.com>
2015-01-06 17:31:29 -0500

    Add .pow folder for POW

--- recipes/infl/recipes/create_folders.rb
+++ recipes/infl/recipes/create_folders.rb
@@ -7,3 +7,8 @@ directory "#{node['sprout']['home']}/.tmuxinator" do
   owner node['current_user']
   action :create
 end
+
+directory "#{node['sprout']['home']}/.pow" do
+  owner node['current_user']
+  action :create
+end
