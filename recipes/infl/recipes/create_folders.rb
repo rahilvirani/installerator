63950946fbf50873e3b2595510257e50a7c11ce8
Rahil Virani <rahil.virani@gmail.com>
2016-02-16 13:23:15 -0800

    Moomshot env

--- recipes/infl/recipes/create_folders.rb
+++ recipes/infl/recipes/create_folders.rb
@@ -2,13 +2,3 @@ directory "#{node['sprout']['home']}/Code" do
   owner node['sprout']['user']
   action :create
 end
-
-directory "#{node['sprout']['home']}/.tmuxinator" do
-  owner node['sprout']['user']
-  action :create
-end
-
-directory "#{node['sprout']['home']}/.pow" do
-  owner node['sprout']['user']
-  action :create
-end
--- recipes/infl/recipes/tmuxinator.rb
+++
@@ -1,9 +0,0 @@
-include_recipe "infl::create_folders"
-
-['development.yml'].each do |file|
-  link "#{node['sprout']['home']}/.tmuxinator/#{file}" do
-    to "#{node['sprout']['home']}/Code/infl/devops/dotfiles/#{file}"
-    owner node['current_user']
-    not_if { File.symlink?("#{node['sprout']['home']}/.tmuxinator/#{file}") }
-  end
-end
